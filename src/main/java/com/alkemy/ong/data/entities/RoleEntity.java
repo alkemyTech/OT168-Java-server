2322061788746c90023eb75d58a6c62e3ff48738
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-16 21:15:04 -0300

    RoleEntity created

---
+++ src/main/java/com/alkemy/ong/data/entities/RoleEntity.java
@@ -0,0 +1,36 @@
+package com.alkemy.ong.data.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles", schema = "alkemy_ong")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+}
