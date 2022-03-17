c589311fa081d1484c2ae3e248aaa3fe04b3955f
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-17 12:10:27 -0300

    lock clause removed of data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,8 @@
 USE `alkemy_ong`;
 
-LOCK TABLES `alkemy_ong`.`roles` WRITE;
 INSERT INTO `alkemy_ong`.`roles` (name, description, createdAt,updateAt) VALUES ('ADMIN', 'System administrator',NOW(),NOW());
-UNLOCK TABLES;
 
-LOCK TABLES `alkemy_ong`.`users` WRITE;
 /*!40000 ALTER TABLE `alkemy_ong`.`users` DISABLE KEYS */;
 INSERT INTO `alkemy_ong`.`users` (firstName, lastName, email, password,photo, createdAt, updateAt, deleted, fk_role)
 VALUES ('Juan','Perez','juan@mail.com',MD5('1234'),'',NOW(),NOW(),0,1);
 /*!40000 ALTER TABLE `alkemy_ong`.`users` ENABLE KEYS */;
-UNLOCK TABLES;
