13750ab73c4228df3dee4ac8e1fcbcd648edd313
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-17 12:48:23 -0300

    MemberEntity created

---
+++ src/main/java/com/alkemy/ong/data/entities/MemberEntity.java
@@ -0,0 +1,46 @@
+package com.alkemy.ong.data.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "members", schema = "alkemy_ong")
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String facebookUrl;
+
+    private String instagramUrl;
+
+    private String linkedinUrl;
+
+    @Column(nullable = false)
+    private String image;
+
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
+    private Boolean deleted;
+}
