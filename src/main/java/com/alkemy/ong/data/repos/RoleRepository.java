5714ba5376d48954ce0a6d117369698c20b78ccf
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-17 09:39:52 -0300

    UserRepository created and RoleRepository fixed

--- src/main/java/com/alkemy/ong/data/repos/RoleRepository.java
+++ src/main/java/com/alkemy/ong/data/repos/RoleRepository.java
@@ -3,5 +3,5 @@ package com.alkemy.ong.data.repos;
 import com.alkemy.ong.data.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Long, RoleEntity> {
+public interface RoleRepository extends JpaRepository<RoleEntity,Long> {
 }
