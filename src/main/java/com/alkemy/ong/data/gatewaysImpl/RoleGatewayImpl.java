fe2850ee89bca0cc4d258e33d70f8ab7a67771d0
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-16 21:39:07 -0300

    RoleGatewayImpl fixed

--- src/main/java/com/alkemy/ong/data/gatewaysImpl/RoleGatewayImpl.java
+++ src/main/java/com/alkemy/ong/data/gatewaysImpl/RoleGatewayImpl.java
@@ -3,4 +3,10 @@ package com.alkemy.ong.data.gatewaysImpl;
 import com.alkemy.ong.domain.usecases.RoleGateway;
 
 public class RoleGatewayImpl implements RoleGateway {
+
+    private final RoleGateway roleGateway;
+
+    public RoleGatewayImpl (RoleGateway roleGateway){
+        this.roleGateway=roleGateway;
+    }
 }
