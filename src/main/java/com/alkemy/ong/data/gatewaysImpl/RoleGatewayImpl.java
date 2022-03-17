e74beed3251886b4b5f30d1dff181c16608106a3
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-16 22:27:33 -0300

    component notation added to RoleGatewayImpl

--- src/main/java/com/alkemy/ong/data/gatewaysImpl/RoleGatewayImpl.java
+++ src/main/java/com/alkemy/ong/data/gatewaysImpl/RoleGatewayImpl.java
@@ -1,7 +1,9 @@
 package com.alkemy.ong.data.gatewaysImpl;
 
 import com.alkemy.ong.domain.usecases.RoleGateway;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RoleGatewayImpl implements RoleGateway {
 
     private final RoleGateway roleGateway;
