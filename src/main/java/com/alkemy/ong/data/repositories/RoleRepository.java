20404245c8e2ab13d062522d262da5edb6bb5f50
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-17 12:15:06 -0300

    name of package respos changed for repositories

--- src/main/java/com/alkemy/ong/data/repositories/RoleRepository.java
+++ src/main/java/com/alkemy/ong/data/repositories/RoleRepository.java
@@ -1,4 +1,4 @@
-package com.alkemy.ong.data.repos;
+package com.alkemy.ong.data.repositories;
 
 import com.alkemy.ong.data.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
