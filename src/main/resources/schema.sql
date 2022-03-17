d6a7ca9e0ebf1361580bf2cdb15c81a2b5d27614
Victor Montivero <victorabel46@gmail.com>
2022-03-17 11:55:59 -0300

    changed schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,15 @@
+CREATE DATABASE IF NOT EXISTS alkemy_ong;
+USE alkemy_ong;
+
+CREATE TABLE alkemy_ong.news (
+news_id BIGINT(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR (20) NOT NULL,
+content VARCHAR (100) NOT NULL,
+image VARCHAR (25) NOT NULL,
+created_at TIMESTAMP,
+updated_at TIMESTAMP,
+deleted TINYINT
+);
 
 CREATE TABLE IF NOT EXISTS organizations (
   id_organization BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
