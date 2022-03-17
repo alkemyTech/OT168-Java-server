8b29bd1ef2d4651ee852c8076779958d96d6f78a
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-17 15:28:54 -0300

    data.sql deleted

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-USE `alkemy_ong`;
-
-INSERT INTO `alkemy_ong`.`roles` (name, description, createdAt,updateAt) VALUES ('ADMIN', 'System administrator',NOW(),NOW());
-
-/*!40000 ALTER TABLE `alkemy_ong`.`users` DISABLE KEYS */;
-INSERT INTO `alkemy_ong`.`users` (firstName, lastName, email, password,photo, createdAt, updateAt, deleted, fk_role)
-VALUES ('Juan','Perez','juan@mail.com',MD5('1234'),'',NOW(),NOW(),0,1);
-/*!40000 ALTER TABLE `alkemy_ong`.`users` ENABLE KEYS */;
