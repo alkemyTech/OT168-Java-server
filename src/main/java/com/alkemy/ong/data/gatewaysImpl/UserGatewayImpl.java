4b1a199885d162fd7ea40c28c923e1ed81585440
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-17 10:39:30 -0300

    UserGatewayImpl created

---
+++ src/main/java/com/alkemy/ong/data/gatewaysImpl/UserGatewayImpl.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.data.gatewaysImpl;
+
+import com.alkemy.ong.domain.usecases.UserGateway;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserGatewayImpl implements UserGateway {
+}
