ae3f24a153ba2b637681d79d1a5b880f4f8723a7
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-18 19:41:50 -0300

    schema.sql fixed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE `alkemy_ong`.`roles`
     `id`          BIGINT(255) NOT NULL AUTO_INCREMENT,
     `name`        VARCHAR(20) NOT NULL,
     `description` VARCHAR(50) DEFAULT NULL,
-    `createdAt`   TIMESTAMP   DEFAULT NOW(),
-    `updateAt`    TIMESTAMP   DEFAULT NOW(),
+    `createdat`   TIMESTAMP   DEFAULT NOW(),
+    `updateat`    TIMESTAMP   DEFAULT NOW(),
     `deleted`     BIT         DEFAULT 0,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
@@ -21,13 +21,13 @@ DROP TABLE IF EXISTS `alkemy_ong`.`users`;
 CREATE TABLE `alkemy_ong`.`users`
 (
     `id`        BIGINT(255)        NOT NULL AUTO_INCREMENT,
-    `firstName` VARCHAR(50)        NOT NULL,
-    `lastName`  VARCHAR(50)        NOT NULL,
+    `firstname` VARCHAR(50)        NOT NULL,
+    `lastname`  VARCHAR(50)        NOT NULL,
     `email`     VARCHAR(30) UNIQUE NOT NULL,
     `password`  VARCHAR(50)        NOT NULL,
     `photo`     VARCHAR(255) DEFAULT NULL,
-    `createdAt` TIMESTAMP    DEFAULT NOW(),
-    `updateAt`  TIMESTAMP    DEFAULT NOW(),
+    `createdat` TIMESTAMP    DEFAULT NOW(),
+    `updateat`  TIMESTAMP    DEFAULT NOW(),
     `deleted`   BIT          DEFAULT 0,
     `roleEntity`   BIGINT(255)        NOT NULL,
     PRIMARY KEY (`id`),
@@ -42,13 +42,13 @@ CREATE TABLE `alkemy_ong`.`members`
 (
     `id`           BIGINT(255)  NOT NULL AUTO_INCREMENT,
     `name`         VARCHAR(50) NOT NULL,
-    `facebookUrl`  VARCHAR(50) DEFAULT NULL,
-    `instagramUrl` VARCHAR(30) DEFAULT NULL,
-    `linkedinUrl`  VARCHAR(50) DEFAULT NULL,
+    `facebookurl`  VARCHAR(50) DEFAULT NULL,
+    `instagramurl` VARCHAR(30) DEFAULT NULL,
+    `linkedinurl`  VARCHAR(50) DEFAULT NULL,
     `image`        VARCHAR(255) NOT NULL,
     `description`  VARCHAR(50) DEFAULT NULL,
-    `createdAt`    TIMESTAMP   DEFAULT NOW(),
-    `updateAt`     TIMESTAMP   DEFAULT NOW(),
+    `createdat`    TIMESTAMP   DEFAULT NOW(),
+    `updateat`     TIMESTAMP   DEFAULT NOW(),
     `deleted`      BIT         DEFAULT 0,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
