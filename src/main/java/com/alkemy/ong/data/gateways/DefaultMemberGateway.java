3d8e9f42c2ccdc43524c8fcfbb7ee67c0be70571
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-18 19:46:06 -0300

    DefaultMemberGateway fixed and method findAll() of MemberGateway implemented

--- src/main/java/com/alkemy/ong/data/gateways/DefaultMemberGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultMemberGateway.java
@@ -1,14 +1,53 @@
 package com.alkemy.ong.data.gateways;
 
+import com.alkemy.ong.domain.members.Member;
 import com.alkemy.ong.domain.members.MemberGateway;
+
+import com.alkemy.ong.data.entities.MemberEntity;
+import com.alkemy.ong.data.repositories.MemberRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class DefaultMemberGateway implements MemberGateway {
 
-    private final MemberGateway memberGateway;
+    private final MemberRepository memberRepository;
 
-    public DefaultMemberGateway(MemberGateway memberGateway){
-        this.memberGateway=memberGateway;
+    public DefaultMemberGateway(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public List<Member> findAll() {
+
+        List<Member> memberList = memberRepository.findAll().stream()
+                .map(memberEntity -> toModel(memberEntity))
+                .collect(Collectors.toList());
+
+        return memberList;
+    }
+
+    private Member toModel(MemberEntity memberEntity) {
+        Member memberModel = Member.builder()
+                .id(memberEntity.getId())
+                .name(memberEntity.getName())
+                .facebookUrl(memberEntity.getFacebookUrl())
+                .instagramUrl(memberEntity.getInstagramUrl())
+                .linkedinUrl(memberEntity.getLinkedinUrl())
+                .image(memberEntity.getImage())
+                .description(memberEntity.getDescription())
+                .createdAt(memberEntity.getCreatedAt())
+                .updateAt(memberEntity.getUpdateAt())
+                .deleted(memberEntity.getDeleted())
+                .build();
+        return memberModel;
     }
 }
+/*
+List<ContactDTO> dto;
+
+           dto = contactService.getContacts().stream()
+                    .map(c -> toDto(c))
+                    .collect(Collectors.toList());*/
