10961292cd83509e89032238733b7521d3c64607
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-19 12:13:52 -0300

    schema.sql fixed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -56,13 +56,15 @@ CREATE TABLE `alkemy_ong`.`members`
 CREATE DATABASE IF NOT EXISTS alkemy_ong;
 USE alkemy_ong;
 
-news_id BIGINT(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR (20) NOT NULL,
-content VARCHAR (100) NOT NULL,
-image VARCHAR (25) NOT NULL,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-updated_at TIMESTAMP,
-deleted TINYINT
+CREATE TABLE alkemy_ong.news
+(
+    news_id    BIGINT(255)  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(20)  NOT NULL,
+    content    VARCHAR(100) NOT NULL,
+    image      VARCHAR(25)  NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted    TINYINT
 );
 
 CREATE TABLE IF NOT EXISTS organizations (
