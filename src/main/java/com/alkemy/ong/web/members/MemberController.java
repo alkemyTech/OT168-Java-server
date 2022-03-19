f1e9d313e939cff4bf9835f743049e7c47fdb45d
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-19 12:53:58 -0300

    parameters deledt of MemberController

--- src/main/java/com/alkemy/ong/web/members/MemberController.java
+++ src/main/java/com/alkemy/ong/web/members/MemberController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 @RestController
-@RequestMapping()
+@RequestMapping
 public class MemberController {
 
     private final MemberService memberService;
