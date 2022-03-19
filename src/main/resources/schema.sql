151438c931af7315c3dbd739de60b01e2e9e0564
Ssuarez <Ssuarez@agm951.aguas.local>
2022-03-19 09:42:56 -0500

    Resolve conflict schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -101,20 +101,4 @@ content VARCHAR (255),
 created_at TIMESTAMP DEFAULT NOW(),
 updated_at TIMESTAMP,
 deleted TINYINT(1)
-<<<<<<< HEAD
-);
-
-CREATE DATABASE IF NOT EXISTS alkemy_ong;
-USE alkemy_ong;
-
-CREATE TABLE IF NOT EXISTS alkemy_ong.categories (
-  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(255),
-  `image` VARCHAR(255),
-  `created_at` TIMESTAMP DEFAULT NOW(),
-  `updated_at` TIMESTAMP,
-  `deleted` TINYINT
-=======
->>>>>>> 8f6ac982248c6b96b7de3e20e3305c296fd8f51f
 );
