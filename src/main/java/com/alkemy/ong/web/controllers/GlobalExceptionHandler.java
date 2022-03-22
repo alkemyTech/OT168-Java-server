d880f28d3befad1dd46451e6e1447afe1542beca
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-21 23:44:36 -0300

    method handleRequestConflict added to GlobalExceptionHandler

--- src/main/java/com/alkemy/ong/web/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/alkemy/ong/web/controllers/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.web.controllers;
 
+import com.alkemy.ong.domain.exceptions.BadRequestException;
 import com.alkemy.ong.domain.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -33,5 +34,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .map(m->m.getDefaultMessage())
                 .collect(toList());
         return new ResponseEntity(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value
+            = { BadRequestException.class, BadRequestException.class })
+    protected ResponseEntity<Object> handleRequestConflict(
+            Exception ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(),
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
