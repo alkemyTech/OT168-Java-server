636322cc9340b51e458094d38ecd1103d07e917e
Eliana Paniagua <elianaprensa@gmail.com>
2022-03-22 16:55:52 -0300

    fixed updateNews

--- src/main/java/com/alkemy/ong/data/gateways/DefaultNewsGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultNewsGateway.java
@@ -30,11 +30,15 @@ public class DefaultNewsGateway implements NewsGateway {
     }
 
     @Override
-    public News updateNews(Long newsId, News news){
+    public News updateNews(Long newsId, News news) {
         NewsEntity newsEntity = newsRepository.findById(newsId).orElseThrow(() -> new ResourceNotFoundException("The ID doesn't exist."));
-        newsRepository.save(toEntity(news));
-        return toModel(newsEntity);
+        newsEntity.setName(newsEntity.getName());
+        newsEntity.setContent(newsEntity.getContent());
+        newsEntity.setImage(newsEntity.getImage());
+        newsEntity.setType(newsEntity.getType());
+        return toModel(newsRepository.save(newsEntity));
     }
+
 
     private NewsEntity toEntity(News news) {
         return NewsEntity.builder()
