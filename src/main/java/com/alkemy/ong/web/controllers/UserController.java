e4d9b7d3debfd66e4f24aef659dd2efda869f7ad
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-23 22:03:24 -0300

    UserController created

---
+++ src/main/java/com/alkemy/ong/web/controllers/UserController.java
@@ -0,0 +1,95 @@
+package com.alkemy.ong.web.controllers;
+
+import com.alkemy.ong.domain.roles.Role;
+import com.alkemy.ong.domain.users.User;
+import com.alkemy.ong.domain.users.UserService;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequestMapping
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok()
+                .body(userService.findAll()
+                .stream()
+                .map(this::toDTO)
+                .collect(toList()));
+    }
+
+    private UserDTO toDTO(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .photo(user.getPhoto())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .role(toRoleDTO(user.getRole()))
+                .build();
+    }
+
+    private RoleDTO toRoleDTO(Role role){
+        return RoleDTO.builder()
+                .id(role.getId())
+                .name(role.getName())
+                .description(role.getDescription())
+                .createdAt(role.getCreatedAt())
+                .updatedAt(role.getUpdatedAt())
+                .build();
+    }
+
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    @Builder
+    private static class UserDTO {
+        private Long id;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private String photo;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createdAt;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime updatedAt;
+        private RoleDTO role;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    @Builder
+    private static class RoleDTO{
+        private Long id;
+        private String name;
+        private String description;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime createdAt;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime updatedAt;
+    }
+}
