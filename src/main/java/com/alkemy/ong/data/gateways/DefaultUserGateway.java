9bfe99a5081b00c26c62f46f498d1fda92c3e4d6
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-23 21:58:58 -0300

    methods findAll and toModel added to DefaultUserGateway

--- src/main/java/com/alkemy/ong/data/gateways/DefaultUserGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultUserGateway.java
@@ -1,15 +1,44 @@
 package com.alkemy.ong.data.gateways;
 
+import com.alkemy.ong.data.entities.UserEntity;
+import com.alkemy.ong.domain.users.User;
 import com.alkemy.ong.domain.users.UserGateway;
 import com.alkemy.ong.data.repositories.UserRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Component
 public class DefaultUserGateway implements UserGateway {
 
     private final UserRepository userRepository;
 
-    public DefaultUserGateway(UserRepository userRepository){
-        this.userRepository=userRepository;
+    public DefaultUserGateway(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(this::toModel)
+                .collect(toList());
+    }
+
+    private User toModel(UserEntity userEntity) {
+        return User.builder()
+                .id(userEntity.getId())
+                .firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName())
+                .email(userEntity.getEmail())
+                .password(userEntity.getPassword())
+                .photo(userEntity.getPhoto())
+                .createdAt(userEntity.getCreatedAt())
+                .updatedAt(userEntity.getUpdatedAt())
+                .deleted(userEntity.getDeleted())
+                .role(DefaultRoleGateway.toModel(userEntity.getRoleEntity()))
+                .build();
     }
 }
