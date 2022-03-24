8f5b46b4edebb42da8ac7c9554f9302cc9c8bce2
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-23 21:59:47 -0300

    method findAll added to UserRepository

--- src/main/java/com/alkemy/ong/data/repositories/UserRepository.java
+++ src/main/java/com/alkemy/ong/data/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.alkemy.ong.data.repositories;
 import com.alkemy.ong.data.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
+    List<UserEntity> findAll();
 }
