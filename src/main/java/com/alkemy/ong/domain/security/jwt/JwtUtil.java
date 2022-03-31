ea935e667138109f890fefd3c39812f041aa4324
Maxi Meza <meza.maximiliano364@gmail.com>
2022-03-31 10:22:18 -0300

    fixed

--- src/main/java/com/alkemy/ong/domain/security/jwt/JwtUtil.java
+++ src/main/java/com/alkemy/ong/domain/security/jwt/JwtUtil.java
@@ -47,7 +47,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))//
                 .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
                 .compact();
     }
