97f0acdda630894035e99cc5cd8e9f85800aac0b
Andrea Reyna <andree.reyna@gmail.com>
2022-04-04 17:55:07 -0300

    getAllContactsTest

--- src/test/java/com/alkemy/ong/web/controllers/ContactControllerTest.java
+++ src/test/java/com/alkemy/ong/web/controllers/ContactControllerTest.java
@@ -1,24 +1,32 @@
 package com.alkemy.ong.web.controllers;
 
+import com.alkemy.ong.data.entities.ContactEntity;
 import com.alkemy.ong.data.repositories.ContactRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.List;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-//@AutoConfigureMockMvc
-@WebMvcTest(ContactController.class)
+@AutoConfigureMockMvc
 class ContactControllerTest {
 
     @Autowired
@@ -32,18 +40,48 @@ class ContactControllerTest {
 
     @BeforeEach
     void setUp() {
-        objectMapper = new ObjectMapper();
+        ContactEntity contact = createContact(3L, "Maria Gonzalez", "341232456", "mariagonzalez@gmail.com", "MessageExample3" );
+
+        List<ContactEntity> contacts = Arrays.asList(
+                createContact(1L, "Juan Perez", "342525156", "juanperez@gmail.com", "MessageExample" ),
+                createContact(2L, "Ignacio Rodriguez", "11334565", "ignacior@gmail.com", "MessageExample2" ));
+//                createEntity(3L, "Maria Gonzalez", "341232456", "mariagonzalez@gmail.com", "MessageExample3" ),
+//                createEntity(4L, "Sofia Martinez", "343567543", "sofiam@gmail.com", "MessageExample4" ));
+        when(contactRepository.findAll()).thenReturn(contacts);
     }
 
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    void getAllContacts() {
-
+    void getAllContacts() throws Exception {
+        mockMvc.perform(get("/contacts")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].name").value("Juan Perez"))
+                .andExpect(jsonPath("$[0].phone").value("342525156"))
+                .andExpect(jsonPath("$[0].email").value("juanperez@gmail.com"))
+                .andExpect(jsonPath("$[0].message").value("MessageExample"))
+                .andExpect(jsonPath("$[1].id").value(2L))
+                .andExpect(jsonPath("$[1].name").value("Ignacio Rodriguez"))
+                .andExpect(jsonPath("$[1].phone").value("11334565"))
+                .andExpect(jsonPath("$[1].email").value("ignacior@gmail.com"))
+                .andExpect(jsonPath("$[1].message").value("MessageExample2"));
     }
 
     @Test
     @WithMockUser(roles = "ADMIN")
     void saveContact() {
+    }
+
+    private ContactEntity createContact(Long id, String name, String phone, String email, String message){
+        return ContactEntity.builder()
+                .id(id)
+                .name(name)
+                .email(email)
+                .message(message)
+                .phone(phone)
+                .build();
     }
 }
