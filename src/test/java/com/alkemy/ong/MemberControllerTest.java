a7278e8e411cee78066c8b79afef11a8943194e1
Maxi Meza <meza.maximiliano364@gmail.com>
2022-04-05 17:03:07 -0300

    testing in progress

--- src/test/java/com/alkemy/ong/MemberControllerTest.java
+++ src/test/java/com/alkemy/ong/MemberControllerTest.java
@@ -2,6 +2,7 @@ package com.alkemy.ong;
 
 import com.alkemy.ong.data.entities.MemberEntity;
 import com.alkemy.ong.data.repositories.MemberRepository;
+import com.alkemy.ong.domain.exceptions.ResourceNotFoundException;
 import com.alkemy.ong.web.controllers.MemberController;
 import com.alkemy.ong.web.controllers.MemberController.MemberDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,12 +23,14 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -70,9 +73,112 @@ public class MemberControllerTest {
                 .andExpect(jsonPath("$.createdAt",is("2022-03-29 18:58:56")))
                 .andExpect(jsonPath("$.updatedAt",is("2022-03-29 18:58:56")));
 
-        ArgumentCaptor<MemberEntity> argumentCaptor = ArgumentCaptor.forClass(MemberEntity.class);
-        verify(memberRepository,times(1)).save(argumentCaptor.capture());
-        verifyNoMoreInteractions(memberRepository);
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void saveBadRequest() throws Exception {
+
+        MemberEntity entityRequest = toEntityTest();
+        MemberDTO memberDTO = toDTOTest();
+
+        entityRequest.setId(null);
+        entityRequest.setName(null);
+
+        memberDTO.setName(null);
+
+        when(memberRepository.save(entityRequest)).thenReturn(null);
+
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(memberDTO)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void deleteSuccess() throws Exception {
+
+        MemberEntity entity = toEntityTest();
+
+        when(memberRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
+
+        mockMvc.perform(delete("/members/{id}",1))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void deleteNotFound() throws Exception {
+
+        when(memberRepository.findById(22l)).thenThrow(new ResourceNotFoundException(22l,"Member"));
+
+        mockMvc.perform(delete("/members/{id}",22))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void updateSuccess() throws Exception {
+
+        MemberEntity entity = toEntityTest();
+        MemberDTO memberDTO = toDTOTest();
+
+        memberDTO.setId(1l);
+
+        when(memberRepository.findById(entity.getId())).thenReturn(Optional.of(entity));
+        when(memberRepository.save(entity)).thenReturn(entity);
+
+        mockMvc.perform(put("/members/{id}",1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(memberDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name",is("James Potter")))
+                .andExpect(jsonPath("$.facebookUrl",is("wwww.facebook/jamespotter.com")))
+                .andExpect(jsonPath("$.instagramUrl",is("wwww.instagram/jamespotter.com")))
+                .andExpect(jsonPath("$.linkedinUrl",is("wwww.linkedin/jamespotter.com")))
+                .andExpect(jsonPath("$.image",is("james.jpg")))
+                .andExpect(jsonPath("$.description",is("Some description about James Potter")))
+                .andExpect(jsonPath("$.createdAt",is("2022-03-29 18:58:56")))
+                .andExpect(jsonPath("$.updatedAt",is("2022-03-29 18:58:56")));
+
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void updateNotFound() throws Exception {
+        MemberDTO memberDTO = toDTOTest();
+
+        when(memberRepository.findById(22l)).thenThrow(new ResourceNotFoundException(22l,"Member"));
+
+        memberDTO.setId(22l);
+
+        mockMvc.perform(put("/members/{id}",22)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(memberDTO)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void updateBadRequest() throws Exception {
+
+        MemberEntity memberEntity = toEntityTest();
+        MemberDTO memberDTO = toDTOTest();
+
+        memberEntity.setName(null);
+
+        memberDTO.setId(1l);
+        memberDTO.setName(null);
+
+        when(memberRepository.findById(memberEntity.getId())).thenReturn(Optional.of(memberEntity));
+        when(memberRepository.save(memberEntity)).thenReturn(null);
+
+        mockMvc.perform(put("/members/{id}",1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(memberDTO)))
+                .andExpect(status().isBadRequest());
     }
 
     private MemberEntity toEntityTest(){
