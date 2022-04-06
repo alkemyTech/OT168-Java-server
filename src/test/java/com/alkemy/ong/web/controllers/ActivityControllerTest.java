60ad28922fe4cffbd58a9e2c104cbc1069fdc03a
Andrea Reyna <andree.reyna@gmail.com>
2022-04-06 11:46:52 -0300

    TestActivity OT168-94

--- src/test/java/com/alkemy/ong/web/controllers/ActivityControllerTest.java
+++ src/test/java/com/alkemy/ong/web/controllers/ActivityControllerTest.java
@@ -4,13 +4,11 @@ import com.alkemy.ong.data.entities.ActivityEntity;
 import com.alkemy.ong.data.repositories.ActivityRepository;
 import com.alkemy.ong.domain.exceptions.ResourceNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
