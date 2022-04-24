101afbadc14e62c6a3061e75de9b6141c51f08a3
Maxi Meza <meza.maximiliano364@gmail.com>
2022-04-24 08:39:09 -0300

    fixed2

--- src/main/java/com/alkemy/ong/factory/lowLevel/VehicleFactoryImp.java
+++ src/main/java/com/alkemy/ong/factory/lowLevel/VehicleFactoryImp.java
@@ -10,19 +10,17 @@ import java.util.Map;
 public class VehicleFactoryImp implements VehicleFactory {
 
     private static Map<VehicleTypes, Vehicle> mapTypes;
-
-    @Override
-    public Vehicle createVehicle(VehicleTypes type) {
-        return newVehicle(type);
-    }
-
+    
     static {
         mapTypes = new EnumMap<>(VehicleTypes.class);
         mapTypes.put(VehicleTypes.MOTORBIKE, new Motorbike());
         mapTypes.put(VehicleTypes.CAR, new Car());
     }
 
-    private static Vehicle newVehicle(VehicleTypes type) {
+    @Override
+    public Vehicle createVehicle(VehicleTypes type) {
         return mapTypes.get(type);
     }
+
+
 }
